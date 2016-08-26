0fd2f1db827029f86fd6528e06ba6d87ebc9fb26
Banks, Martin <anotherbanksy@gmail.com>
2016-08-26 14:37:38 +1000

    add intro and article text overlay

--- src/flex100/app.js
+++ src/flex100/app.js
@@ -1,10 +1,67 @@
 (()=>{
 
-	var state = {
+	var windowHeight = window.innerHeight
+	
+
+	function gridTemplate(){
+		let imageGrid = []
+		for(let i = 0; i<state.count; i++){
+			imageGrid.push(`<div id='grid_${i}' class="gridItem" style="background-image: url('${state.image.path}${state.image.name}')"></div>`)
+		}
+		return imageGrid.join('')
+	}
+
+	function articleOverlay(){
 
 	}
 
+	function chapterTemplate(){
+		return state.article.chapters.map((chapter, index)=>{
+			return `
+				<section id='chapter${index}' class="chapter set${chapter.align}" style="height:${windowHeight}px">
+					<h3>${chapter.headline}</h3>
+					<p>${chapter.text}</p>
+				</section>
+			`
+		}).join('')
+		
+	}
 
+	function introTemplate(){
+		let article = state.article
+		return `
+			<section id="introContainer" style="height:${windowHeight}px">
+				<div>
+					<h1>${article.headline}</h1>
+					<hr class="white short">
+					<h2>${article.intro}</h2>
+				</div>		
+			</section>
+		`
+	}
+
+
+	function renderPage(){
+		return `
+			<section id="imageGridContainer">
+				${gridTemplate()}
+			</section>
+
+			<section id="articleContainer">
+				<article>
+					${introTemplate()}
+					${chapterTemplate()}
+				</article>
+			</section>
+		`
+	}
+
+
+
+
+
+	// render page
+	renderTemplate(renderPage(), state.container)
 
 	
 })()
---
+++ src/flex100/state.js
@@ -0,0 +1,45 @@
+var state = {
+		container: document.getElementById('appContainer'),
+		count: 100,
+		image: {
+			path: '../images/',
+			name: '4x3.jpg'
+		},
+		article: {
+			headline: 'Lorem ipsum dolor',
+			intro: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, vero!',
+			chapters: [
+				{
+					headline: 'Lorem ipsum dolor sit amet',
+					text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur sit voluptatum labore incidunt ullam dolor in eaque debitis repudiandae expedita, quidem libero fuga, quos rem corrupti, vitae reiciendis. A, autem!',
+					align: 'left',
+				},
+				{
+					headline: 'Lorem ipsum dolor sit amet',
+					text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur sit voluptatum labore incidunt ullam dolor in eaque debitis repudiandae expedita, quidem libero fuga, quos rem corrupti, vitae reiciendis. A, autem!',
+					align: 'left',
+				},
+				{
+					headline: 'Lorem ipsum dolor sit amet',
+					text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur sit voluptatum labore incidunt ullam dolor in eaque debitis repudiandae expedita, quidem libero fuga, quos rem corrupti, vitae reiciendis. A, autem!',
+					align: 'left',
+				},
+				{
+					headline: 'Lorem ipsum dolor sit amet',
+					text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur sit voluptatum labore incidunt ullam dolor in eaque debitis repudiandae expedita, quidem libero fuga, quos rem corrupti, vitae reiciendis. A, autem!',
+					align: 'left',
+				},
+				{
+					headline: 'Lorem ipsum dolor sit amet',
+					text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur sit voluptatum labore incidunt ullam dolor in eaque debitis repudiandae expedita, quidem libero fuga, quos rem corrupti, vitae reiciendis. A, autem!',
+					align: 'left',
+				},
+				{
+					headline: 'Lorem ipsum dolor sit amet',
+					text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur sit voluptatum labore incidunt ullam dolor in eaque debitis repudiandae expedita, quidem libero fuga, quos rem corrupti, vitae reiciendis. A, autem!',
+					align: 'left',
+				}
+			]
+		}
+	
+	}
