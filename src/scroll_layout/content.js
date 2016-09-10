04b06d35fd490ebc7288fc4778544861243fd190
Martin Banks <anotherbanksy@gmail.com>
2016-09-11 01:24:33 +1000

    update content js file with dummy info structure

--- src/scroll_layout/content.js
+++ src/scroll_layout/content.js
@@ -5,10 +5,10 @@ const article = {
 
 	chapters: [
 		{
-			title: '',
+			title: 'Article headline',
 			text: [
-				'',
-				''
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam dignissimos quod, amet recusandae, magni molestias unde adipisci aliquid iure, sunt et esse cumque. Ipsam et velit, pariatur rerum, commodi earum.',
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam dignissimos quod, amet recusandae, magni molestias unde adipisci aliquid iure, sunt et esse cumque. Ipsam et velit, pariatur rerum, commodi earum.'
 			],
 			image: {
 				name: '',
